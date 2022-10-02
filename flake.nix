9f7ab979ca77f6b248a5ff8fdfb6b8d4424fce4f
Hideaki Kawai <kayhide@gmail.com>
2022-10-02 07:40:34 +0200

    Simplify python setup

--- flake.nix
+++ flake.nix
@@ -12,34 +12,21 @@
           pkgs = import inputs.nixpkgs { inherit system; overlays = [ overlay ]; };
 
           my-python = pkgs.python3.withPackages (p: with p; [
-            pip
-            setuptools
-            virtualenv
-            wheel
+            numpy
+            matplotlib
           ]);
-
-          lib-path = with pkgs; lib.makeLibraryPath [
-            stdenv.cc.cc
-            zlib
-          ];
-
-          dev-env = pkgs.buildEnv {
-            name = "dev-env";
-            paths = with pkgs; [
-              my-python
-              my-python.pkgs.pip
-              gnumake
-            ];
-          };
 
         in
         {
           devShell = pkgs.mkShell {
             buildInputs = with pkgs; [
-              dev-env
-            ];
+              my-python
 
-            LD_LIBRARY_PATH = lib-path;
+              entr
+              fd
+              feh
+              gnumake
+            ];
           };
         };
     in
